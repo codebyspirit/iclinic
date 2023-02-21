4d64797be32e688b8a3b876d357ca548db1283bd
kre <kreadou@gmail.com>
2023-02-21 18:06:20 +0000

    03

--- iclinic/settings.py
+++ iclinic/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
 INSTALLED_APPS += [
     'rest_framework',
 
+    'authentication',
+
 
     'parametre',
-
 
 ]
 
@@ -129,8 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
