c7f92b57b2c5b7a6e95ef50c5a909a9a03c254fc
kre <kreadou@gmail.com>
2023-03-13 13:43:52 +0000

    update now

--- connexion/views.py
+++ connexion/views.py
@@ -48,7 +48,7 @@ class UserView(APIView):
 			raise AuthenticationFailed('Unauthenticated')
 
 		try:
-			payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+			payload = jwt.decode(token, 'secret', algorithms=['HS256'])
 		except jwt.ExpiredSignatureError:
 			raise AuthenticationFailed('Unauthenticated')
 
